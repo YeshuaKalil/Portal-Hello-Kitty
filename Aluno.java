90aad936aadd5fbba88c1b2bb5ac085265944e7d
Kyon <139816800+JoaoAntP@users.noreply.github.com>
2025-10-08 09:48:27 -0300

    Create Aluno.java

---
+++ Aluno.java
@@ -0,0 +1,33 @@
+package extra;
+
+public class Aluno extends Pessoa {
+    protected String matricula;
+    protected double nota1;
+    protected double nota2;
+
+    public Aluno(String nome, String cpf, String telefone, String matricula, double nota1, double nota2) {
+        super(nome, cpf, telefone);
+        this.matricula = matricula;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public Aluno(String nome, String cpf, String matricula, double nota1, double nota2) {
+        super(nome, cpf);
+        this.matricula = matricula;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public double calcularMedia() {
+        return (nota1 + nota2) / 2;
+    }
+
+    public void exibir() {
+        super.exibir();
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Média: " + calcularMedia());
+    }
+}
